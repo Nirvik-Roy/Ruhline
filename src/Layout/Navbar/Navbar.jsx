465ccac13c3af9e0c480ca5e8a43f45b8348c3cb
Nirvik Roy <nirvikroy802@gmail.com>
2025-12-18 22:41:46 +0530

    home link add

--- src/Layout/Navbar/Navbar.jsx
+++ src/Layout/Navbar/Navbar.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './Navbar.css'
 import logo from '../../assets/Images/Frame 1984078480.svg'
 import NavbarLinks from './NavbarLinks'
+import { useNavigate } from 'react-router-dom'
 const Navbar = () => {
+    const navigate = useNavigate()
     return (
         <>
             <div className='navbar_wrapper_main'>
                 <div className='navbar_content_wrapper all_Container'>
-                    <img className='nav_logo' src={logo} />
+                    <img onClick={(()=>navigate('/'))} className='nav_logo' src={logo} />
                     <NavbarLinks/>
                 </div>
             </div>
