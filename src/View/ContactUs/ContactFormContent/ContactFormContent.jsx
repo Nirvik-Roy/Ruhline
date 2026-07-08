e5721c24c572fd71b299cd441970913b2f699404
Nirvik Roy <nirvikroy802@gmail.com>
2026-07-08 22:46:23 +0530

    contact us cms from frontend is done

--- src/View/ContactUs/ContactFormContent/ContactFormContent.jsx
+++ src/View/ContactUs/ContactFormContent/ContactFormContent.jsx
@@ -1,177 +1,246 @@
-import React, { useState, useEffect } from 'react'
-import './ContactFormContent.css'
-import Input from '../../../Components/Inputs/Input'
-import Textarea from '../../../Components/Inputs/Textarea'
-import Button from '../../../Components/Button/Button'
-import phone from '../../../assets/Images/Frame 831.svg'
-import mail from '../../../assets/Images/.svg'
-import toast from 'react-hot-toast'
-import axios from 'axios'
-import Loaders from '../../../Components/Loaders/Loaders'
-import { getPhoneCountryCode } from '../../../utils/location'
-import DashboardLoader from '../../../Components/Loaders/DashboardLoader'
-const ContactFormContent = () => {
-    const [loading, setLoading] = useState(false);
-    const [postLoading, setpostLoading] = useState(false)
-    const [emailErrormessage, setEmailerrorMessage] = useState('');
-    const [errors, setErrors] = useState([])
-    const [phoneData, setPhoneData] = useState([]);
+import React, { useState, useEffect } from "react";
+import "./ContactFormContent.css";
+import Input from "../../../Components/Inputs/Input";
+import Textarea from "../../../Components/Inputs/Textarea";
+import Button from "../../../Components/Button/Button";
+import phone from "../../../assets/Images/Frame 831.svg";
+import mail from "../../../assets/Images/.svg";
+import toast from "react-hot-toast";
+import axios from "axios";
+import Loaders from "../../../Components/Loaders/Loaders";
+import { getPhoneCountryCode } from "../../../utils/location";
+import DashboardLoader from "../../../Components/Loaders/DashboardLoader";
+const ContactFormContent = ({ contactData }) => {
+  const [loading, setLoading] = useState(false);
+  const [postLoading, setpostLoading] = useState(false);
+  const [emailErrormessage, setEmailerrorMessage] = useState("");
+  const [errors, setErrors] = useState([]);
+  const [phoneData, setPhoneData] = useState([]);
 
-    const [formData, setformData] = useState({
-        name: '',
-        email: '',
-        phone: '',
-        phone_country_code_id: '5',
-        message: ''
-    })
-    useEffect(() => {
-        const fetchPhone = async () => {
-            setLoading(true)
-            try {
-                const result = await getPhoneCountryCode();
-                setPhoneData(result);
-            } catch (err) {
-                console.log(err)
-            } finally {
-                setLoading(false)
-            }
-        }
-        fetchPhone()
-    }, [])
+  const [formData, setformData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    phone_country_code_id: "5",
+    message: "",
+  });
+  useEffect(() => {
+    const fetchPhone = async () => {
+      setLoading(true);
+      try {
+        const result = await getPhoneCountryCode();
+        setPhoneData(result);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchPhone();
+  }, []);
 
-
-    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    const ValidateEmail = (email) => {
-        if (!email) {
-            return setEmailerrorMessage('* Email is Required')
-        }
-        if (!emailRegex.test(email)) {
-            return setEmailerrorMessage('* Please Enter a vaild email address')
-        }
-        return setEmailerrorMessage('');
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const ValidateEmail = (email) => {
+    if (!email) {
+      return setEmailerrorMessage("* Email is Required");
     }
-
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        if (name === 'email') {
-            ValidateEmail(value)
-        }
-        setformData({
-            ...formData,
-            [name]: value
-        })
+    if (!emailRegex.test(email)) {
+      return setEmailerrorMessage("* Please Enter a vaild email address");
     }
-    const postContactForm = async (e) => {
-        e.preventDefault()
-        const { name, email, phone, phone_country_code_id, message } = formData;
-        if (name != '' && email != '' && phone != '' && phone_country_code_id != '' && message != '') {
-            setpostLoading(true)
-            try {
-                const res = await axios.post(`${import.meta.env.VITE_BASE_URL}/contact`, formData);
-                if (res.data.success == true) {
-                    toast.success(res.data?.message || 'Contact form submitted succesfully');
-                    return res.data.data
-                }
-            } catch (err) {
-                toast.error(err.response?.data?.message);
-                setErrors(err.response.data.errors)
-            } finally {
-                setpostLoading(false)
-            }
-        } else {
-            toast.error('Plz enter all the fileds...')
-        }
+    return setEmailerrorMessage("");
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    if (name === "email") {
+      ValidateEmail(value);
     }
-    return (
-        <>
-            <div className='contact_us_form_wrapper' style={{
-                position: 'relative'
-            }}>
-                {loading && <DashboardLoader />}
+    setformData({
+      ...formData,
+      [name]: value,
+    });
+  };
+  const postContactForm = async (e) => {
+    e.preventDefault();
+    const { name, email, phone, phone_country_code_id, message } = formData;
+    if (
+      name != "" &&
+      email != "" &&
+      phone != "" &&
+      phone_country_code_id != "" &&
+      message != ""
+    ) {
+      setpostLoading(true);
+      try {
+        const res = await axios.post(
+          `${import.meta.env.VITE_BASE_URL}/contact`,
+          formData,
+        );
+        if (res.data.success == true) {
+          toast.success(
+            res.data?.message || "Contact form submitted succesfully",
+          );
+          return res.data.data;
+        }
+      } catch (err) {
+        toast.error(err.response?.data?.message);
+        setErrors(err.response.data.errors);
+      } finally {
+        setpostLoading(false);
+      }
+    } else {
+      toast.error("Plz enter all the fileds...");
+    }
+  };
+  return (
+    <>
+      <div
+        className="contact_us_form_wrapper"
+        style={{
+          position: "relative",
+        }}
+      >
+        {loading && <DashboardLoader />}
 
-                {!loading && <>
-                    <h1 className='all_heading2'>Get in Touch</h1>
-                    <p>Enim tempor eget pharetra facilisis sed maecenas adipiscing. Eu leo molestie vel, ornare non id blandit netus.</p>
-                    <form className='contact_us_form_wrapper_main'>
-                        <div>
-                            <Input onChange={handleChange} name={'name'} value={
-                                formData.name
-                            } label={'Name'} required={true} placeholder={'Enter your name'} />
+        {!loading && (
+          <>
+            <h1 className="all_heading2">
+              {contactData?.heading || "Get in Touch"}
+            </h1>
+            {contactData?.subheading && <p>{contactData?.subheading}</p>}
+            <form className="contact_us_form_wrapper_main">
+              <div>
+                <Input
+                  onChange={handleChange}
+                  name={"name"}
+                  value={formData.name}
+                  label={"Name"}
+                  required={true}
+                  placeholder={"Enter your name"}
+                />
 
-                            <small style={{
-                                fontSize: '0.7rem',
-                                display: 'block',
-                                marginTop: '5px',
-                                color: 'red'
-                            }}>{errors.name && errors.name[0]}</small>
-                        </div>
-                        <div>
-                            <Input name={'email'} value={formData.email} onChange={handleChange} label={'Email'} required={true} placeholder={'Enter your email'} />
-                            <small style={{
-                                marginLeft: '15px',
-                                fontSize: '11px',
-                                marginTop: '-15px',
-                                color: 'rgba(255, 0, 0, 1)',
-                                cursor: 'pointer'
-                            }}>{errors.email ? errors.email[0] : emailErrormessage}</small>
-                        </div>
-                        <div className='input_form confirm_input_form'>
-                            <label>Phone no<span>*</span></label>
-                            <div className='phone_input_Wrapper656'>
-                                <select name='phone_country_code_id' onChange={handleChange} value={formData.phone_country_code_id} style={{
-                                    border: 'none',
-                                    borderRight: '2px solid #000',
-                                    outline: 'none'
-                                }}>
-                                    {phoneData?.map((e,) => (
-                                        <option key={e.id} value={e.id}>+{e.phone_code}</option>
-                                    ))}
-                                </select>
-                                <input onChange={handleChange} name='phone' value={formData.phone} placeholder='Enter phone number' />
-                            </div>
+                <small
+                  style={{
+                    fontSize: "0.7rem",
+                    display: "block",
+                    marginTop: "5px",
+                    color: "red",
+                  }}
+                >
+                  {errors.name && errors.name[0]}
+                </small>
+              </div>
+              <div>
+                <Input
+                  name={"email"}
+                  value={formData.email}
+                  onChange={handleChange}
+                  label={"Email"}
+                  required={true}
+                  placeholder={"Enter your email"}
+                />
+                <small
+                  style={{
+                    marginLeft: "15px",
+                    fontSize: "11px",
+                    marginTop: "-15px",
+                    color: "rgba(255, 0, 0, 1)",
+                    cursor: "pointer",
+                  }}
+                >
+                  {errors.email ? errors.email[0] : emailErrormessage}
+                </small>
+              </div>
+              <div className="input_form confirm_input_form">
+                <label>
+                  Phone no<span>*</span>
+                </label>
+                <div className="phone_input_Wrapper656">
+                  <select
+                    name="phone_country_code_id"
+                    onChange={handleChange}
+                    value={formData.phone_country_code_id}
+                    style={{
+                      border: "none",
+                      borderRight: "2px solid #000",
+                      outline: "none",
+                    }}
+                  >
+                    {phoneData?.map((e) => (
+                      <option key={e.id} value={e.id}>
+                        +{e.phone_code}
+                      </option>
+                    ))}
+                  </select>
+                  <input
+                    onChange={handleChange}
+                    name="phone"
+                    value={formData.phone}
+                    placeholder="Enter phone number"
+                  />
+                </div>
 
-                            <small style={{
-                                fontSize: '0.7rem',
-                                display: 'block',
-                                marginTop: '5px',
-                                color: 'red'
-                            }}>{errors?.phone && errors?.phone[0]}</small>
-                        </div>
-                        <div>
+                <small
+                  style={{
+                    fontSize: "0.7rem",
+                    display: "block",
+                    marginTop: "5px",
+                    color: "red",
+                  }}
+                >
+                  {errors?.phone && errors?.phone[0]}
+                </small>
+              </div>
+              <div>
+                <Textarea
+                  name={"message"}
+                  onChange={handleChange}
+                  value={formData.message}
+                  label={"Message "}
+                  required={true}
+                  placeholder={"Type your message"}
+                />
+                <small
+                  style={{
+                    fontSize: "0.7rem",
+                    display: "block",
+                    marginTop: "5px",
+                    color: "red",
+                  }}
+                >
+                  {errors?.message && errors?.message[0]}
+                </small>
+              </div>
+              <div onClick={(e) => postContactForm(e)}>
+                <Button
+                  loading={postLoading}
+                  loadingText="Sending..."
+                  children={"Send"}
+                />
+              </div>
 
-                            <Textarea name={'message'} onChange={handleChange} value={formData.message} label={'Message '} required={true} placeholder={'Type your message'} />
-                            <small style={{
-                                fontSize: '0.7rem',
-                                display: 'block',
-                                marginTop: '5px',
-                                color: 'red'
-                            }}>{errors?.message && errors?.message[0]}</small>
-                        </div>
-                        <div onClick={((e) => postContactForm(e))}>
-                            <Button loading={postLoading} loadingText='Sending...' children={'Send'} />
-                        </div>
+              <div className="phone_email_wrapper">
+                <div className="phone_wrapper">
+                  <img src={phone} />
+                  <div>
+                    <h5>PHONE</h5>
+                    <span>03 5432 1234</span>
+                  </div>
+                </div>
+                <div className="phone_wrapper">
+                  <img src={mail} />
+                  <div>
+                    <h5>EMAIL</h5>
+                    <span>info@marcc.com.au</span>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </>
+        )}
+      </div>
+    </>
+  );
+};
 
-                        <div className='phone_email_wrapper'>
-                            <div className='phone_wrapper'>
-                                <img src={phone} />
-                                <div>
-                                    <h5>PHONE</h5>
-                                    <span>03 5432 1234</span>
-                                </div>
-                            </div>
-                            <div className='phone_wrapper'>
-                                <img src={mail} />
-                                <div>
-                                    <h5>EMAIL</h5>
-                                    <span>info@marcc.com.au</span>
-                                </div>
-                            </div>
-                        </div>
-                    </form>
-                </>}
-            </div>
-        </>
-    )
-}
-
-export default ContactFormContent
+export default ContactFormContent;
--- src/View/ContactUs/ContactUsForm/ContactUsForm.jsx
+++ src/View/ContactUs/ContactUsForm/ContactUsForm.jsx
@@ -1,22 +1,37 @@
-import React from 'react'
-import './ContactUsForm.css'
-import ContactFormContent from '../ContactFormContent/ContactFormContent'
-import img from '../../../assets/Images/Rectangle 453.png'
-import FAQ from '../FAQ/FAQ'
+import React, { useEffect, useState } from "react";
+import "./ContactUsForm.css";
+import ContactFormContent from "../ContactFormContent/ContactFormContent";
+import img from "../../../assets/Images/Rectangle 453.png";
+import FAQ from "../FAQ/FAQ";
+import { getAllCmsData } from "../../../utils/cms";
+import { useOutletContext } from "react-router-dom";
 const ContactUsForm = () => {
-    return (
-        <>
-            <div className='contact_us_wrapper'>
-                <div className='all_Container contact_us_content_wrapper'>
-                    <div className='contact_us_img'>
-                        <img src={img}/>
-                    </div>
-                    <ContactFormContent />
-                </div>
-                <FAQ/>
-            </div>
-        </>
-    )
-}
+  const [contactData, setcontactData] = useState({});
+  const { sitesettingsData } = useOutletContext();
+  const getContactData = async () => {
+    const response = await getAllCmsData("/contact-page");
+    if (response?.success) {
+      setcontactData(response?.data || {});
+    }
+  };
+  useEffect(() => {
+    getContactData();
+  }, []);
 
-export default ContactUsForm
+  console.log(contactData);
+  return (
+    <>
+      <div className="contact_us_wrapper">
+        <div className="all_Container contact_us_content_wrapper">
+          <div className="contact_us_img">
+            <img src={contactData?.side_image || img} />
+          </div>
+          <ContactFormContent contactData={contactData} />
+        </div>
+        <FAQ />
+      </div>
+    </>
+  );
+};
+
+export default ContactUsForm;
