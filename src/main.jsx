97603974cc902bab9d5601391e1f75940234b19d
Nirvik Roy <nirvikroy802@gmail.com>
2026-09-18 00:14:40 +0530

    added methodSpoofing for PUT to POST

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import store from '../Store/Store'
+import './CustomAxios/methodSpoofing'
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
